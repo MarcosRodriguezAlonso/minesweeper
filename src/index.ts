e2fb77fd15d18c14901724870e79fa1a70c983b0
Marcos Rodríguez <e.marcosra@gmail.com>
2024-04-19 13:29:21 +0200

    Import styles, export funtion and variables, add netlify command

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-import { Tile, Board } from "./types";
+import { Board } from "./types";
+import { Tile } from "./types";
 
-const tile: Tile = {
+export const tile: Tile = {
   hasMine: false,
   minesArround: 0,
 };
 
-module.exports = tile;
-
-const board: Board = {
+export const board: Board = {
   rows: 5,
   columns: 5,
 };
 
-module.exports = board;
-
-export const createBoard = (rows: number, columns: number) => board;
+export const createBoard = (rows: number, columns: number): Board => board;
